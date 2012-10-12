718a13dda74c2b3bf514f1cee71879e98b146f71
rachyandco <alexis.roussel@partipirate.ch>
2012-10-12 10:47:07 +0200

    add english language file

---
+++ lang/lang.en.php
@@ -0,0 +1,66 @@
+<?
+/************************************************************************/
+/* AgorActu 0.1 - RSS agregator with anonymous comments			*/
+/* Copyright (c)2012 Swiss Pirate Party www.partipirate.ch		*/
+/* ---------------------------------------------------------------------*/
+/* This is lang.en.php - Read the readme.txt file for more information	*/
+/************************************************************************/
+
+/* 
+------------------
+Language: English
+------------------
+*/
+
+$lang = array();
+
+// Menu
+$lang['MENU_ARTICLES'] = 'News';
+$lang['MENU_LASTARTICLES'] = 'Last News';
+$lang['MENU_MOSTCOMMENTED'] = 'Most commented news';
+$lang['MENU_LASTCOMMENTED'] = 'Last comments';
+$lang['MENU_FEEDLIST'] = 'Feeds list';
+$lang['MENU_ABOUT'] = 'About';
+$lang['MENU_SEARCH'] = 'Search';
+
+$lang['MODAL_CLOSE'] = 'Close';
+$lang['MODAL_ADDCOMMENT'] = 'Add a comment';
+$lang['MODAL_COMMENTEXT'] = 'Your comment';
+$lang['MODAL_NAME'] = 'Name or Pseudo';
+$lang['MODAL_SAVE'] = 'Save';
+$lang['MODAL_CANCEL'] = 'Cancel';
+
+// List
+$lang['LIST_REACT'] = 'Comment!';
+$lang['LIST_COMMENTS'] = 'Comments';
+$lang['LIST_COMMENTS2'] = 'comment(s)';
+$lang['LIST_FLAGCOMMENT'] = 'Report a comment';
+$lang['LIST_FLAGQUESTION'] = 'Are you sure you want to report this comment ?';
+$lang['LIST_FLAG'] = 'Flagged';
+$lang['LIST_COMMENTWASFLAGGED'] = 'This comment has been flagged';
+$lang['LIST_SHOW'] = 'Show';
+$lang['LIST_ANON'] = 'Anonymous';
+
+// Admin
+
+$lang['ADMIN_NOCOMMENTS'] = 'No comments to review.';
+$lang['ADMIN_COMMENTSAVAILABLE'] = 'Comments to review';
+$lang['ADMIN_AUTHOR'] = 'Name';
+$lang['ADMIN_DELETE'] = 'Delete';
+$lang['ADMIN_PUTBACK'] = 'Revert';
+
+$lang['ADMIN_PARAM_CONFIG'] = 'Configuration';
+$lang['ADMIN_PARAM_TITLE'] = 'Site title';
+$lang['ADMIN_PARAM_SUBTITLE'] = 'Site subtitle';
+$lang['ADMIN_PARAM_TITLELABEL'] = 'Site title in browser head';
+$lang['ADMIN_PARAM_NBARTICLES'] = 'Number of news per page';
+
+$lang['ADMIN_PARAM_AUTOUPDATE'] = 'Automatic update (deactivate if a cronjob is present)';
+$lang['ADMIN_PARAM_ACTIVATED'] = 'Activated';
+$lang['ADMIN_PARAM_DEACTIVATED'] = 'Deactivated';
+$lang['ADMIN_PARAM_SITELANGUAGE'] = 'Site Language';
+$lang['ADMIN_PARAM_SAVE'] = 'Save';
+$lang['ADMIN_PARAM_CANCEL'] = 'Cancel';
+
+$lang['FEED_LIST'] = 'Used RSS Feeds';
+?>
